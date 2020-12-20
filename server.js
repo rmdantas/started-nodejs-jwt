8d93b147202696fb3bb2ea04a781114115c9440c
Rodrigo Machado Dantas <rmdantas@users.noreply.github.com>
2020-12-20 13:42:55 -0300

    Fix previous commit #7

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv-safe').config();
-
-const app = require('./src/app')
-const http = require('http');
-const port = process.env.PORT;
-
-http.createServer(app).listen(port, () => {
-    console.log(`Service started and listening to the door ${port}`);
-});
